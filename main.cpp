9fd067c030a76dc9ad9c06b90a93b4621cf27363
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-01-24 10:25:00 +0000

    Deploy Porphyrion/otus.lesson.4 to github.com/Porphyrion/otus.lesson.4.git:gh-pages

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-//
+/**
+ * ... первая строчка ...
+ * ... вторая строчка ...
+ */
 int main(int argc, char const *argv[]){
 
 	std::cout<< "tak_tak_tak";
--- version.h
+++ version.h
@@ -1,3 +1,3 @@
 #pragma once 
 
-#define PROJECT_VERSION_PATCH 16
+#define PROJECT_VERSION_PATCH 17
