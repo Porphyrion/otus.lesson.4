7782394a623748c021bbced754a56f780922516c
Ivan <ivanbasurman88@yandex.ru>
2018-01-28 00:07:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -48,35 +48,17 @@ typename std::enable_if<is_vector_or_list<T>::value, void>::type print_ip(T t)
 	std::cout<<std::endl;
 }
 
-
-/*template<class T>
-typename std::enable_if<std::is_vector<T>::value, void>::type foo(T t)
-{	
-	for(auto i = t.begin(); i!=t.end(); i++){
-		if(i!=t.begin()){
-			std::cout<<"."<<*i;
-		}
-		else{
-			std::cout<<*i;
-		}
-	}
-	std::cout<<std::endl;
-}*/
-
-
 int main(int argc, char const *argv[]){
 	
 
-	std::vector<int> f{1,24,54,65};
-	print_ip(char(-1));
-	print_ip(short(0));
-	print_ip(int(2130706433));
-	print_ip(long(8875824491850138409));
-	print_ip(std::string{"13.45.65.89"});
-	print_ip(f);
-	print_ip(std::list<std::string>{"243", "895", "543", "146"});
-
-
+	
+	print_ip(char(-1));///< Печать адреса как char(-1)
+	print_ip(short(0));///< Печать адреса как short(0)
+	print_ip(int(2130706433));///<Печать адреса как int(2130706433)
+	print_ip(long(8875824491850138409));///<Печать адреса как long(8875824491850138409)
+	print_ip(std::string{"13.45.65.89"});///<Печать адреса как std::string
+	print_ip(std::vector<int>{1,24,54,65};);///<Печать адреса как std::vector
+	print_ip(std::list<std::string>{"243", "895", "543", "146"});///<Печать адреса как std::list
 
 	return 0;
 }
