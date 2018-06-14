8de5f620eb1600dc0eb5f1675f33fb93d3ae5d4d
Ivan <ivanbasurman88@yandex.ru>
2018-06-14 16:50:50 +0300

    Добавил комментарии для doxygen

--- mytypetraits.h
+++ mytypetraits.h
@@ -3,25 +3,31 @@
 \brief Заголовочный файл с определением метакласса
 
 Данный файл содержит в себе определение шаблонного
-метакласса, используемого для проверки типа 
+метакласса, используемого для проверки типа
 */
 
 #include <vector>
 #include <list>
-
+/*
+Проверяет, является ли тип std::vector или std::list
+*/
 template<typename T>
 struct is_vector_or_list{
 	static const bool value = false;
 };
-
+/*
+Специализация is_vector_or_list для std::vector
+*/
 template<typename T>
 struct is_vector_or_list<std::vector<T>>
 {
-	static const bool value = true;	
+	static const bool value = true;
 };
-
+/*
+Специализация is_vector_or_list для std::list
+*/
 template<typename T>
 struct is_vector_or_list<std::list<T>>
 {
-	static const bool value = true;	
+	static const bool value = true;
 };
